8f1d4855e12c8fb2a92e41171293126349f8466c
Syed Huma <ss1516@duke.edu>
2024-12-13 18:21:40 -0500

    changes

--- mylib/extract.py
+++ mylib/extract.py
@@ -4,9 +4,11 @@ import requests
 def extract(
     url="https://raw.githubusercontent.com/nogibjj/Mu-Niu-Pandas-Descriptive-Statistics-Script/main/student_performance.csv",
     file_path="data/student_performance.csv",
+    path="data/student_performance.csv"
 ):
     """ "Extract a url to a file path"""
     with requests.get(url) as r:
         with open(file_path, "wb") as f:
             f.write(r.content)
-    return file_path
+    return path
+
